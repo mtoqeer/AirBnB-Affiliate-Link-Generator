08ea30e5a5f8bc42d0616fc74a765c9b0dff8423
Toqeer <toqeer.94@gmail.com>
2019-12-13 00:48:12 +0500

    Complete Code V-1.0.0

---
+++ app.js
@@ -0,0 +1,49 @@
+document.getElementById('msg').style.display = 'none';
+
+document.getElementById('airbnb-button').addEventListener('click', function(e){
+
+    const link = document.getElementById('airbnb');
+    const msgdiv = document.getElementById('msg');
+
+    if(link.value === ''){
+        msgdiv.style.display = 'block';
+        msgdiv.innerHTML = 'Please Enter Link!!';
+        setTimeout(() => {
+            msgdiv.style.display = 'none';
+            msgdiv.innerHTML = '';
+        }, 3000);
+    } else{
+        copyToClipboard(`http://airbnb.pvxt.net/c/1956590/264339/4273?u=${link.value}`);
+        link.value = '';
+        msgdiv.style.display = 'block';
+        msgdiv.innerHTML = 'Link Has Been Generated and Copied Succfullly!';
+        setTimeout(() => {
+            msgdiv.style.display = 'none';
+            msgdiv.innerHTML = '';
+        }, 3000);
+    }
+
+    e.preventDefault();
+})
+
+
+const copyToClipboard = str => {
+  const el = document.createElement('textarea');  // Create a <textarea> element
+  el.value = str;                                 // Set its value to the string that you want copied
+  el.setAttribute('readonly', '');                // Make it readonly to be tamper-proof
+  el.style.position = 'absolute';                 
+  el.style.left = '-9999px';                      // Move outside the screen to make it invisible
+  document.body.appendChild(el);                  // Append the <textarea> element to the HTML document
+  const selected =            
+    document.getSelection().rangeCount > 0        // Check if there is any content selected previously
+      ? document.getSelection().getRangeAt(0)     // Store selection if found
+      : false;                                    // Mark as false to know no selection existed before
+  el.select();                                    // Select the <textarea> content
+  document.execCommand('copy');                   // Copy - only works as a result of a user action (e.g. click events)
+  document.body.removeChild(el);                  // Remove the <textarea> element
+  if (selected) {                                 // If a selection existed before copying
+    document.getSelection().removeAllRanges();    // Unselect everything on the HTML document
+    document.getSelection().addRange(selected);   // Restore the original selection
+  }
+};
+
